0e126f53eea4ed76b25e7e8320e9745882d7eed5
Domico Carlo <w.domicocarlo@gmail.com>
2025-10-25 23:13:05 +1100

    Trigger deployment

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,7 +4,7 @@
     <p>Welcome to our digital library! We're dedicated to providing a vast collection of books and resources to our community.</p>
   </div>
 </template>
-
+ 
 <script setup>
 // No script needed for now
 </script>
